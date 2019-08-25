1f1ae6f7ef711bb03cb4b808269b648a0f2ace5b
Sabina Shvetsova <sabinabo@ya.ru>
2019-08-25 22:41:38 +0400

    Добавила защиту от некорректного ввода пользователем имени файла

--- main.py
+++ main.py
@@ -18,20 +18,25 @@ if __name__ == "__main__":
 
     if mode == '1':
         filename = input('Введите имя файла, куда будут записаны скретч-коды: ')
+        directory, file = os.path.split(filename)
 
-        scratch_codes_count = get_value('scratch_codes_count')
-        serial_number_length = len(str(int(first_serial_number) + scratch_codes_count).zfill(first_serial_number_length))
-        scratch_codes = scratch.generate(serial_number_length, hash_type, hash_length, scratch_codes_count)
-        write_json_data(scratch_codes, filename)
+        if not os.path.isdir(filename) and not directory or os.path.exists(directory):
+            scratch_codes_count = get_value('scratch_codes_count')
+            serial_number_length = len(str(int(first_serial_number) + scratch_codes_count).zfill(first_serial_number_length))
+            scratch_codes = scratch.generate(serial_number_length, hash_type, hash_length, scratch_codes_count)
+            write_json_data(scratch_codes, filename)
+
+        else:
+            print('Неправильное имя файла.')
 
     if mode == '2':
         filename = input('Введите имя файла, откуда будут взяты скретч-коды для проверки: ')
 
-        if os.path.exists(filename):
+        if os.path.isfile(filename):
             scratch_codes = get_json_data(filename)
             serial_number_length = len(str(int(first_serial_number) + len(scratch_codes)).zfill(first_serial_number_length))
             checked_codes = scratch.check(scratch_codes, serial_number_length, hash_type, hash_length)
             print(checked_codes)
 
         else:
-            print('Проверяемый файл не существует.')
+            print('Неправильное имя файла.')
--- utils.py
+++ utils.py
@@ -1,12 +1,17 @@
 import json
+import sys
 
 
 CONFIG_FILE_PATH = 'config.json'
 
 def get_json_data(path):
-    with open(path) as f:
-        data = json.load(f)
-    return data
+    try:
+        with open(path) as f:
+            data = json.load(f)
+        return data
+    except ValueError:
+        print('В файле содержатся данные в неправильном формате.')
+        sys.exit()
 
 def write_json_data(data, path):
     with open(path, 'w') as f:
